8e61c44c1f5cac6e95a96c1e8d48873ca2bb7b0a
Ilyas <ilyas@titan.com>
2023-08-10 13:30:08 +0300

    first commit

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from .models import Advertisement
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 
 
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction', 'updated_date']
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction', 'updated_date', 'get_image']
     list_filter = ['auction', 'created_dt']
     actions = ['make_auction_as_false', 'make_auction_as_true']
 
+    def get_image(selfself, obj):
+        return mark_safe(f'<img src={obj.image.url} width="90" height="90"')
+
+    get_image.short_description = 'Изображение'
 
     @admin.action(description='Отменить возможность торга')
     def make_auction_as_false(selfself, request, queryset):
@@ -21,15 +27,13 @@ class AdvertisementAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             'Общее',
-            {'fields': ('title', 'description')}
+            {'fields': ('title', 'description', 'image')}
         ),
         (
             'Финансы',
             {'fields': ('price', 'auction'), 'classes': ['collapse']}
         )
     )
-
-
 
 
 admin.site.register(Advertisement, AdvertisementAdmin)
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib import admin
-from django.utils import timezone
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Advertisement(models.Model):
     title = models.CharField("Заголовок", max_length=128)
@@ -10,6 +11,9 @@ class Advertisement(models.Model):
     auction = models.BooleanField("Торг", help_text='Отметьте если торг уместен')
     created_dt = models.DateTimeField(auto_now_add=True)
     updated_dt = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('изображение', upload_to='advertisements/')
+
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
